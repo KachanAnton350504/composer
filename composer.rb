8169c5323c4ceaaf8098fe78ec950434f7022bd5
Anton Kachan <anton.kachan@monostacklabs.com>
2018-06-28 00:32:41 +0300

    fds dd

--- composer.rb
+++ composer.rb
@@ -1,6 +1,15 @@
-require "./lib/command"
-require "./lib/commands/postgres_config"
+def source_paths
+  Array(super) +
+      [File.expand_path(File.dirname(__FILE__))]
+end
+
+directory 'lib', 'builder'
+require "./builder/command"
+require "./builder/commands/postgres_config"
+
 
 # Dir["./lib/commands/*.rb"].each {|file| require_relative file }
 # require "lib/commands/postgres_config"
 PostgresConfig.new(self).execute
+# puts ApplicationController
+run "rm -r builder"
