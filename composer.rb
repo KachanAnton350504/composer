2e2f8a5d6f2068343caeef60be6d223803a03fbc
Anton Kachan <anton.kachan@monostacklabs.com>
2018-06-28 00:45:59 +0300

    fds dd

--- composer.rb
+++ composer.rb
@@ -1,15 +1,16 @@
-def source_paths
-  Array(super) +
-      [File.expand_path(File.dirname(__FILE__))]
-end
+# def source_paths
+#   Array(super) +
+#       ["./composer"]
+# end
+run "git clone https://github.com/KachanAnton350504/composer.git"
 
-directory 'lib', 'builder'
-require "./builder/command"
-require "./builder/commands/postgres_config"
+# directory 'lib', 'builder'
+require "./composer/lib/command"
+require "./composer/lib/commands/postgres_config"
 
 
 # Dir["./lib/commands/*.rb"].each {|file| require_relative file }
 # require "lib/commands/postgres_config"
 PostgresConfig.new(self).execute
 # puts ApplicationController
-run "rm -r builder"
+run "rm -r composer"
