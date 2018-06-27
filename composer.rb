1fb480f50411bd541e22039f97d04187ecf9156e
Anton Kachan <anton.kachan@monostacklabs.com>
2018-06-28 00:05:31 +0300

    fds d

--- composer.rb
+++ composer.rb
@@ -1,5 +1,5 @@
-require_relative "./lib/command"
-require_relative "./lib/commands/postgres_config"
+require "./lib/command"
+require "./lib/commands/postgres_config"
 
 # Dir["./lib/commands/*.rb"].each {|file| require_relative file }
 # require "lib/commands/postgres_config"
